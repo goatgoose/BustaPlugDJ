aee95c0627f60f1172998a7804bf2b37d8ef4f5c
Sam Clark <sam.clark1142@gmail.com>
2014-06-05 06:02:50 -0400

    initial templating

---
+++ src/main/java/com.goatgoose.bustaplugdj/BustaPlugDJ.java
@@ -0,0 +1,24 @@
+package com.goatgoose.bustaplugdj;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BustaPlugDJ extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String commandLabel, String[] args) {
+        return true;
+    }
+
+}
